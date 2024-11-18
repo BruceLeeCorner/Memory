31b396c06d188155be81307fdc1c5a1a0f16f753
Bruce Lee <bruceleecorner@gmail.com>
2024-11-18 23:12:16 +0800

    backup

---
+++ Memory/Memory/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory
+{
+    internal class Node
+    {
+        public string Id { get; set; }
+        public string ChainId { get; set; }
+        public Node Parent { get; set; }
+        public List<Node> Children { get; set; }
+        public bool IsLeaf { get; set; }
+        public bool IsRoot { get; set; }
+        public bool IsFile { get; set; }
+    }
+}
